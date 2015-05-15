a59ab98a4ec78fa3f533b266bc40ccfd4fd8dc3f
Gu Jingxiao <gujingxiao@gmail.com>
2015-05-15 19:29:05 +0800

    + DAO Framework + ChildUnitDAO.java

---
+++ src/cn/edu/fudan/dao/BaseDAO.java
@@ -0,0 +1,60 @@
+package cn.edu.fudan.dao;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Dawnwords on 2015/5/15.
+ */
+public abstract class BaseDAO<T> {
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ServletContext context;
+
+    public BaseDAO(HttpServlet servlet) {
+        this.context = servlet.getServletContext();
+    }
+
+    private Connection connect() throws SQLException {
+        String host = context.getInitParameter("DBHost");
+        String port = context.getInitParameter("DBPort");
+        String db = context.getInitParameter("DBName");
+        String user = context.getInitParameter("DBUser");
+        String pass = context.getInitParameter("DBPass");
+        String url = String.format("jdbc:mysql://%s:%s/%s", host, port, db);
+        return DriverManager.getConnection(url, user, pass);
+    }
+
+    public T getResult() {
+        Connection conn = null;
+        T result = null;
+
+        try {
+            conn = connect();
+            result = processData(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    protected abstract T processData(Connection connection) throws Exception;
+}
---
+++ src/cn/edu/fudan/dao/ChildUnitDAO.java
@@ -0,0 +1,37 @@
+package cn.edu.fudan.dao;
+
+import cn.edu.fudan.bean.Unit;
+
+import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dawnwords on 2015/5/15.
+ */
+public class ChildUnitDAO extends BaseDAO<List<Unit>> {
+    private String unitId;
+
+    public ChildUnitDAO(HttpServlet servlet, String unitId) {
+        super(servlet);
+        this.unitId = unitId;
+    }
+
+    @Override
+    protected List<Unit> processData(Connection connection) throws Exception {
+        List<Unit> result = new ArrayList<>();
+        String sql = "select id,name from tab_unit where fatherid =?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, unitId);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            String id = String.valueOf(rs.getInt(1));
+            String name = rs.getString(2);
+            result.add(new Unit(id, name));
+        }
+        return result;
+    }
+}
