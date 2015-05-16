7bce1d9c7daa843650fefe245b412c6ebe156648
Gu Jingxiao <gujingxiao@gmail.com>
2015-05-16 17:52:19 +0800

    ChildUnitServlet GET -> POST fit BaseServlet Framework

---
+++ src/cn/edu/fudan/request/CunitRequest.java
@@ -0,0 +1,12 @@
+package cn.edu.fudan.request;
+
+/**
+ * Created by Dawnwords on 2015/5/16.
+ */
+public class CunitRequest {
+    public final int unitId;
+
+    public CunitRequest(int unitId) {
+        this.unitId = unitId;
+    }
+}
--- src/cn/edu/fudan/servlet/ChildUnitServlet.java
+++ src/cn/edu/fudan/servlet/ChildUnitServlet.java
@@ -2,25 +2,25 @@ package cn.edu.fudan.servlet;
 
 import cn.edu.fudan.bean.Unit;
 import cn.edu.fudan.dao.ChildUnitDAO;
+import cn.edu.fudan.request.CunitRequest;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
  * Created by Dawnwords on 2015/5/15.
  */
 @WebServlet(name = "ChildUnitServlet", urlPatterns = "/cunit")
-public class ChildUnitServlet extends BaseServlet<List<Unit>> {
+public class ChildUnitServlet extends BaseServlet<CunitRequest, List<Unit>> {
 
     public ChildUnitServlet() {
-        super(GET);
+        super(POST, CunitRequest.class);
     }
 
     @Override
-    protected List<Unit> processRequest(HttpServletRequest request) throws Exception {
-        String unitId = request.getParameter("unitId");
-        unitId = unitId == null || "".equals(unitId) ? "0" : unitId;
+    protected List<Unit> processRequest(CunitRequest request) throws Exception {
+        String unitId = request == null ? "0" : String.valueOf(request.unitId);
         return new ChildUnitDAO(this, unitId).getResult();
     }
+
 }
