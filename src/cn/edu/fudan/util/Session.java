a11103eff9980704885f881df809cd3a16e99f0e
Gu Jingxiao <gujingxiao@gmail.com>
2015-06-07 20:56:40 +0800

    + HttpServletRequest in constructor of Session + redirect & reqeustURI in Session getUser() -> user() setUser(u) -> user(u)

--- src/cn/edu/fudan/util/Session.java
+++ src/cn/edu/fudan/util/Session.java
@@ -3,6 +3,8 @@ package cn.edu.fudan.util;
 import cn.edu.fudan.bean.User;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Dawnwords on 2015/6/5.
@@ -10,16 +12,30 @@ import javax.servlet.http.HttpServletRequest;
 public class Session {
     private static final String USER = "user";
     private HttpServletRequest request;
+    private HttpServletResponse response;
 
-    public Session(HttpServletRequest request) {
+    public Session(HttpServletRequest request, HttpServletResponse response) {
         this.request = request;
+        this.response = response;
     }
 
-    public User getUser() {
+    public User user() {
         return (User) request.getSession().getAttribute(USER);
     }
 
-    public void setUser(User user) {
+    public void user(User user) {
         request.getSession().setAttribute(USER, user);
+    }
+
+    public void redirect(String url) {
+        try {
+            response.sendRedirect(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String requestURI() {
+        return request.getRequestURI();
     }
 }
