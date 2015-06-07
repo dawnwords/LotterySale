e9fb7ead9e590ebb9e2caae83591587109b38fab
Gu Jingxiao <gujingxiao@gmail.com>
2015-06-07 20:58:43 +0800

    + annotation in LogoutServlet

--- src/cn/edu/fudan/servlet/LogoutServlet.java
+++ src/cn/edu/fudan/servlet/LogoutServlet.java
@@ -2,9 +2,12 @@ package cn.edu.fudan.servlet;
 
 import cn.edu.fudan.util.Session;
 
+import javax.servlet.annotation.WebServlet;
+
 /**
  * Created by Dawnwords on 2015/6/7.
  */
+@WebServlet(name = "LogoutServlet" ,urlPatterns = {"/logout"})
 public class LogoutServlet extends BaseServlet<Void, Void> {
     public LogoutServlet() {
         super(POST, Void.class);
