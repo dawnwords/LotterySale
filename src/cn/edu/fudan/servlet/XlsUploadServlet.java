edbc7dc80531597e9b5566cff67d966216df035a
Gu Jingxiao <gujingxiao@gmail.com>
2015-06-22 22:53:55 +0800

    fix bug: forget to catch Exception for AddTableDAO

--- src/cn/edu/fudan/servlet/XlsUploadServlet.java
+++ src/cn/edu/fudan/servlet/XlsUploadServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -33,6 +34,8 @@ public class XlsUploadServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+        String responseString;
         if (request.getContentType().contains("multipart/form-data")) {
             try {
                 FileItem fileItem = prepareUploadFile().parseRequest(request).get(0);
@@ -40,8 +43,7 @@ public class XlsUploadServlet extends HttpServlet {
                 String table = sheet.getSheetName();
                 Iterator<Row> it = sheet.iterator();
                 List<String> fieldList = getFieldList(it);
-                List<String> fails = new LinkedList<>();
-                int success = 0;
+                Result result = new Result();
                 while (it.hasNext()) {
                     Row row = it.next();
                     AddTableRequest addRequest = new AddTableRequest(table);
@@ -49,25 +51,36 @@ public class XlsUploadServlet extends HttpServlet {
                     for (String field : fieldList) {
                         addRequest.addAdd(field, row.getCell(i++).getStringCellValue());
                     }
-                    if (new AddTableDAO(this, addRequest).getResult() < 0) {
-                        fails.add(addRequest.addValue());
-                    } else {
-                        success++;
-                    }
-                    sendSuccess(response, new Result(success, fails));
+                    addResult(result, addRequest);
                 }
+                responseString = BaseServlet.gson.toJson(result);
             } catch (FileUploadException e) {
-                sendError(response, "文件上传失败");
+                responseString = String.format("{error:'%s'}", "文件上传失败");
             } catch (InvalidFormatException e) {
-                sendError(response, "xls文件格式不正确");
+                responseString = String.format("{error:'%s'}", "xls文件格式不正确");
             }
         } else {
-            sendError(response, "没有文件上传");
+            responseString = String.format("{error:'%s'}", "没有文件上传");
+        }
+        PrintWriter writer = response.getWriter();
+        writer.write(responseString);
+        writer.flush();
+    }
+
+    private void addResult(Result result, AddTableRequest addRequest) {
+        int id = -1;
+        try {
+            id = new AddTableDAO(this, addRequest).getResult();
+        } catch (Exception ignore) {
+        }
+        if (id < 0) {
+            result.addFail(addRequest.addValue());
+        } else {
+            result.success++;
         }
     }
 
     private void sendSuccess(HttpServletResponse response, Result result) throws IOException {
-        response.setContentType("application/json");
         PrintWriter writer = response.getWriter();
         writer.write(BaseServlet.gson.toJson(result));
         writer.flush();
@@ -77,9 +90,12 @@ public class XlsUploadServlet extends HttpServlet {
         private int success;
         private List<String> fails;
 
-        public Result(int success, List<String> fails) {
-            this.success = success;
-            this.fails = fails;
+        private Result() {
+            this.fails = new ArrayList<>();
+        }
+
+        private void addFail(String fail) {
+            fails.add(fail);
         }
     }
 
@@ -99,12 +115,5 @@ public class XlsUploadServlet extends HttpServlet {
         ServletFileUpload upload = new ServletFileUpload(factory);
         upload.setSizeMax(MAX_FILE_SIZE);
         return upload;
-    }
-
-    private void sendError(HttpServletResponse response, String msg) throws IOException {
-        response.setContentType("application/json");
-        PrintWriter writer = response.getWriter();
-        writer.write(String.format("{error:%s}", msg));
-        writer.flush();
     }
 }
