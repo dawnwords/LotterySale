72f157de97a9933e0aae5b5afe548befca1e0e05
Gu Jingxiao <gujingxiao@gmail.com>
2015-07-16 09:30:16 +0800

    YearDAO returns [{year:[month]}]

--- src/cn/edu/fudan/dao/YearDAO.java
+++ src/cn/edu/fudan/dao/YearDAO.java
@@ -5,24 +5,32 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 /**
  * Created by Dawnwords on 2015/7/15.
  */
-public class YearDAO extends BaseDAO<List<String>> {
+public class YearDAO extends BaseDAO<HashMap<String, ArrayList<String>>> {
     public YearDAO(HttpServlet servlet) {
         super(servlet);
     }
 
     @Override
-    protected List<String> processData(Connection connection) throws Exception {
-        List<String> result = new ArrayList<>();
-        String sql = "SELECT DISTINCT saleyear FROM tab_sales";
+    protected HashMap<String, ArrayList<String>> processData(Connection connection) throws Exception {
+        HashMap<String, ArrayList<String>> result = new HashMap<>();
+        String sql = "SELECT DISTINCT saleyear,salemonth FROM tab_sales";
         PreparedStatement ps = connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
+        rs.next();
         while (rs.next()) {
-            result.add(rs.getString(1));
+            String year = rs.getString(1);
+            String month = rs.getString(2);
+            ArrayList<String> months = result.get(year);
+            if (months == null) {
+                months = new ArrayList<>();
+                result.put(year, months);
+            }
+            months.add(month);
         }
         return result;
     }
--- src/cn/edu/fudan/servlet/YearServlet.java
+++ src/cn/edu/fudan/servlet/YearServlet.java
@@ -4,19 +4,20 @@ import cn.edu.fudan.dao.YearDAO;
 import cn.edu.fudan.util.Session;
 
 import javax.servlet.annotation.WebServlet;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Dawnwords on 2015/7/15.
  */
 @WebServlet(name = "YearServlet", urlPatterns = "/year")
-public class YearServlet extends BaseServlet<Void, List<String>> {
+public class YearServlet extends BaseServlet<Void, HashMap<String, ArrayList<String>>> {
     public YearServlet() {
         super(GET, Void.class);
     }
 
     @Override
-    protected List<String> processRequest(Void request, Session session) throws Exception {
+    protected HashMap<String, ArrayList<String>> processRequest(Void request, Session session) throws Exception {
         return new YearDAO(this).getResult();
     }
 }
